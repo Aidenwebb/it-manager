91f13fcb8354d43137ae98db4885fc0599b0468c
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2022-11-11 19:11:18 +0000

    Add Migration for Institutions to use Guids

---
+++ JsgItManager.Data/Migrations/20221111190434_InstitutionsGuidId.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace JsgItManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstitutionsGuidId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "id",
+                table: "institutions",
+                type: "uuid",
+                nullable: false,
+                defaultValueSql: "gen_random_uuid()",
+                oldClrType: typeof(int),
+                oldType: "integer")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "institutions",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldDefaultValueSql: "gen_random_uuid()")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
---
+++ JsgItManager.Data/Migrations/20221111190434_InstitutionsGuidId.sql
@@ -0,0 +1,20 @@
+﻿START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM __ef_migrations_history WHERE "migration_id" = '20221111190434_InstitutionsGuidId') THEN
+        ALTER TABLE institutions ALTER COLUMN id DROP IDENTITY;
+        ALTER TABLE institutions ALTER COLUMN id TYPE uuid using gen_random_uuid();
+        ALTER TABLE institutions ALTER COLUMN id SET DEFAULT (gen_random_uuid());
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM __ef_migrations_history WHERE "migration_id" = '20221111190434_InstitutionsGuidId') THEN
+    INSERT INTO __ef_migrations_history (migration_id, product_version)
+    VALUES ('20221111190434_InstitutionsGuidId', '7.0.0');
+    END IF;
+END $EF$;
+COMMIT;
+
