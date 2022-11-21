2eb936264763344565c6d2cd4412208d00baf87a
t-sharp <24504688+t-sharp@users.noreply.github.com>
2022-11-21 19:51:29 +0100

    Added an IRegistrarRepository interface and its implementation.

---
+++ src/ItManager.Core/Repositories/IRegistrarRepository.cs
@@ -0,0 +1,27 @@
+﻿using ItManager.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItManager.Core.Repositories;
+
+/// <summary>
+/// Represents the interface for handling and enumerating registrars. For each registrar the list of encapsulated domains can be retrieved.
+/// </summary>
+public interface IRegistrarRepository : IRepository<Registrar, Guid>
+{
+    /// <summary>
+    /// Asynchronously gets all registrars in the database and ensures that all domains of the registrars are loaded.
+    /// </summary>
+    /// <returns>The <see cref="Task"/> that asynchronously retrieves a list of all registrars with their domains.</returns>
+    Task<List<Registrar>> GetAllWithDomainsAsync();
+
+    /// <summary>
+    /// Asynchronously ensures that the navigation property <see cref="Registrar.Domains"/> of <paramref name="registrar"/> had been loaded.
+    /// </summary>
+    /// <param name="registrar">The <see cref="Registrar"/> instance from which all domains should be loaded.</param>
+    /// <returns>The <see cref="Task"/> that loads all domains for the given <paramref name="registrar"/>; or nothing if all domains are already loaded.</returns>
+    Task EnsureDomainsAreLoadedAsync(Registrar registrar);
+}
---
+++ src/ItManager.Data/Repositories/RegistrarRepository.cs
@@ -0,0 +1,31 @@
+﻿using ItManager.Core.Models;
+using ItManager.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItManager.Data.Repositories;
+
+/// <summary>
+/// The default implementation of the <see cref="IRegistrarRepository"/> interface.
+/// </summary>
+public class RegistrarRepository : Repository<Registrar, Guid>, IRegistrarRepository
+{
+    /// <summary>
+    /// Initializes a new instance of this <see cref="RegistrarRepository"/> instance that uses the provided <paramref name="context"/> as data store.
+    /// </summary>
+    /// <param name="context">The database context to use.</param>
+    public RegistrarRepository(JsgItManagerDbContext context) : base(context)
+    {
+    }
+
+    /// <inheritdoc/>
+    public Task EnsureDomainsAreLoadedAsync(Registrar registrar)
+    {
+        return Context.Entry(registrar).Collection(r => r.Domains).LoadAsync();
+    }
+
+    /// <inheritdoc/>
+    public Task<List<Registrar>> GetAllWithDomainsAsync()
+    {
+        return Context.Registrars.Include(r => r.Domains).ToListAsync();
+    }
+}
